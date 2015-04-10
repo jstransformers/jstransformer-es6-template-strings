c7c653dff414c8bfc9f57eca59cd26cacfd1f790
Rob Loach <robloach@gmail.com>
2015-04-10 12:50:29 -0400

    docs(~): Update documentation

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ exports.inputFormats = ['es6-template-strings', 'es6-template-string'];
 exports.outputFormat = 'html';
 
 exports.compile = function (str, options) {
-  // Compile the template string using `es6-templates`.
   var compiled = compile(str);
 
-  // Build the render function, which takes the locals.
   return function(locals) {
     return resolveToString(compiled, locals);
   };
