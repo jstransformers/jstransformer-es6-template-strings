91c92a7e5ab5b0e76c82f4871f9e59528fdce3a3
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-var compile = require('es6-template-strings/compile');
-var resolveToString = require('es6-template-strings/resolve-to-string');
+var compile = require('es6-template-strings/compile')
+var resolveToString = require('es6-template-strings/resolve-to-string')
 
-exports.name = 'es6-template-strings';
-exports.inputFormats = ['es6-template-strings', 'es6-template-string'];
-exports.outputFormat = 'html';
+exports.name = 'es6-template-strings'
+exports.inputFormats = ['es6-template-strings', 'es6-template-string']
+exports.outputFormat = 'html'
 
 exports.compile = function (str, options) {
-  var compiled = compile(str);
+  var compiled = compile(str)
 
   return function(locals) {
-    return resolveToString(compiled, locals);
-  };
-};
+    return resolveToString(compiled, locals)
+  }
+}
